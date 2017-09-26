cad34189b27c3d16860f38fc6a2dee471044ed8c
TJ Loughry <tjloughry@gmail.com>
2017-09-26 16:28:39 -0500

    objectives complete not bonus

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,48 @@
-$(document).on("ready", function(){
+$(document).ready(function(){
+
+  $.ajax({
+    method: 'GET',
+    url: 'http://api.giphy.com/v1/gifs/trending',
+    type: 'GET /v1/gifs/trending',
+    dataType: 'json',
+    data: $("form").serialize(),
+    success: onSuccess,
+    error: onError
+  });
+
+
+$(".form-inline").submit(function(event) {
+  event.preventDefault();
+  $(".gif-gallery").html('')
+  console.log('hello?')
+
+  $.ajax({
+    method: 'GET',
+    url: 'http://api.giphy.com/v1/gifs/search?',
+    type: 'GET /v1/gifs/trending',
+    dataType: 'json',
+    data: $("form").serialize(),
+    success: onSuccess,
+    error: onError
+
+  })
+
+});
+
+  function onSuccess(responseData) {
+  $.each(responseData.data, function(i){
+  // console.log(responseData.data[0].images.fixed_height.url);
+  var imgData = responseData.data[i].images.fixed_height.url;
+  $(".gif-gallery").append('<img src=' + imgData + '>');
+});
+}
+  function onError(responseData){
+  console.log("error");
+}
+
+  /* Act on the event */
+
+
+
 
 });
